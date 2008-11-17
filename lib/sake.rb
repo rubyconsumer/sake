4c1e4403e83dd4f23274b4d220305cfd7230879b
postmodern <postmodern.mod3@gmail.com>
2008-11-16 23:09:27 -0800

    Moving some files around.

--- lib/sake.rb
+++ lib/sake.rb
@@ -19,12 +19,12 @@ rescue LoadError
   exit
 end
 
-require File.join(File.dirname(__FILE__), 'version')
-require File.join(File.dirname(__FILE__), 'tasks_array')
-require File.join(File.dirname(__FILE__), 'tasks_file')
-require File.join(File.dirname(__FILE__), 'task')
-require File.join(File.dirname(__FILE__), 'help')
-require File.join(File.dirname(__FILE__), 'pastie')
+require 'sake/version'
+require 'sake/tasks_array'
+require 'sake/tasks_file'
+require 'sake/task'
+require 'sake/help'
+require 'sake/pastie'
 
 ##
 # Show all Sake tasks (but no local Rake tasks), optionally only those matching a pattern.
@@ -343,75 +343,7 @@ class Sake
   end
 end
 
-module Rake # :nodoc: all
-  class Application
-    ##
-    # Show the tasks as 'sake' tasks.
-    def printf(*args)
-      args[0].sub!('rake', 'sake') if args[0].is_a? String
-      super
-    end
-
-    ##
-    # Show tasks that don't have comments'
-    def display_tasks_and_comments(tasks = nil, pattern = nil, display_hidden = nil)
-      tasks ||= self.tasks
-
-      if pattern ||= options.show_task_pattern
-        tasks = tasks.select { |t| t.name[pattern] || t.comment.to_s[pattern] }
-      end
-
-      width = tasks.collect { |t| t.name.length }.max
-
-      tasks.each do |t|
-        comment = "   # #{t.comment}" if t.comment
-        if display_hidden
-          printf "sake %-#{width}s#{comment}\n", t.name
-        else
-          printf "sake %-#{width}s#{comment}\n", t.name if t.name && t.comment
-        end
-      end
-    end
-    alias_method :show, :display_tasks_and_comments
-
-    ##
-    # Run Sake even if no Rakefile exists in the current directory.
-    alias_method :sake_original_have_rakefile, :have_rakefile
-    def have_rakefile(*args)
-      @rakefile ||= ''
-      sake_original_have_rakefile(*args) || true
-    end
-  
-    ##
-    # Accept only one task, unlike Rake, to make passing arguments cleaner.
-    alias_method :sake_original_collect_tasks, :collect_tasks
-    def collect_tasks
-      sake_original_collect_tasks
-      @top_level_tasks = [@top_level_tasks.first]
-    end
-  end
-end
-
-##
-# Hacks which give us "Rakefile".is_file? 
-class String # :nodoc: 
-  def is_file?
-    File.exists? self
-  end
-end
-
-class Nil # :nodoc: 
-  def is_file?
-    false
-  end
-
-  # under the evil
-  def method_missing(*args, &block)
-    super
-  end
-end
-
-def die(*message) # :nodoc: 
+def die(*message) # :nodoc:
   puts message 
   exit
 end
